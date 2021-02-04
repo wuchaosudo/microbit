845a421a7a074b7a2f1b4571dc966572f81447b9
wuchaosudo <62998236+wuchaosudo@users.noreply.github.com>
2021-02-04 19:10:11 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,52 @@
-basic.showIcon(IconNames.Heart)
+input.onButtonPressed(Button.A, function () {
+    basic.showString("A")
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.showIcon(IconNames.Heart)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("B")
+})
+basic.showIcon(IconNames.Target)
+basic.showIcon(IconNames.SmallDiamond)
 basic.forever(function () {
-    basic.showIcon(IconNames.Happy)
-    music.startMelody(music.builtInMelody(Melodies.Birthday), MelodyOptions.Once)
-    basic.showIcon(IconNames.Square)
-    basic.showIcon(IconNames.Diamond)
-    basic.showIcon(IconNames.SmallSquare)
-    basic.showIcon(IconNames.SmallDiamond)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . # . .
-        . . . . .
-        . . . . .
-        `)
+    while (input.isGesture(Gesture.Shake)) {
+        basic.showIcon(IconNames.Sad)
+    }
+    while (input.isGesture(Gesture.LogoUp)) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    }
+    while (input.isGesture(Gesture.LogoDown)) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    }
+    while (input.isGesture(Gesture.TiltLeft)) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    }
+    while (input.isGesture(Gesture.TiltRight)) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    }
 })
